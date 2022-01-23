f323732f87ce053c40439b03984b324e5789d351
Vladyslav48 <63408113+Vladyslav48@users.noreply.github.com>
2022-01-23 17:33:21 +0100

    Update Connection.java

--- networkSimulator/src/net/sim/Connection.java
+++ networkSimulator/src/net/sim/Connection.java
@@ -2,14 +2,21 @@ package net.sim;
 
 
 public abstract class Connection implements  EventHandler{
-    //Vlad
-    private int connectionIDIncrement = 0;
-    private int id = connectionIDIncrement++;
-    private Device device1;
-    private Device device2;
-    private float failureRate;
-    private int speed;
-
+    //Vlad & Nikita
+    private static int connectionIDIncrement = 0;
+    public int id = connectionIDIncrement++;
+    public Device device1;
+    public Device device2;
+    public ConnectionType type;
+    public float failureRate;
+    public int speed;
+    public Connection(Device _device1, Device _device2, ConnectionType _type){
+        device1 = _device1;
+        device2 = _device2;
+        id = connectionIDIncrement;
+        type = _type;
+        connectionIDIncrement++;
+    }
     public void setFailureRate(float failureRate) {
         this.failureRate = failureRate;
     }
