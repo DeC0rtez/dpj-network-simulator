7c864369fb6e6c8d739d364e1446803e1a727a38
Nedr <ndrpowerr@gmail.com>
2022-01-23 18:04:02 +0300

    IPAddress addition

--- networkSimulator/src/net/sim/IPAddress.java
+++ networkSimulator/src/net/sim/IPAddress.java
@@ -1,4 +1,22 @@
 package net.sim;
 
 public class IPAddress {
+    public String ipStr = "";
+    public IPAddress(String ip){
+        ipStr = ip;
+    }
+    public byte[] GetBytes(){
+        byte[] bytes = {0,0,0,0};
+        if (ipStr.length() > 0){
+            String[] ipStrings = ipStr.split("\\.");
+            if (ipStrings.length == 4){
+                for (int i = 0; i < ipStrings.length; i++){
+                    byte byt = (byte) Integer.parseInt(ipStrings[i]);
+                    bytes[i] = byt;
+                }
+            }
+        }
+        return bytes;
+    }
 }
+// Nikita Druzhkov
--- networkSimulator/src/net/sim/Main.java
+++ networkSimulator/src/net/sim/Main.java
@@ -1,8 +1,8 @@
 package net.sim;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test");
+
+
     }
 }
