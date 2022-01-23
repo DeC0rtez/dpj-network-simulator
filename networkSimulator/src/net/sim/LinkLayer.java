87c25ba24f3d91051e29b39e94670691fc89c91e
M4RS-music <65905992+M4RS-music@users.noreply.github.com>
2022-01-23 17:19:01 +0100

    Created basic structure of OSI 3 Layers
    Added constructors that force creation of sub layers
    Added getters for sub layers in link and network
    
    --MARTIN JANDA 5-5:20

--- networkSimulator/src/net/sim/LinkLayer.java
+++ networkSimulator/src/net/sim/LinkLayer.java
@@ -3,4 +3,13 @@ package net.sim;
 public class LinkLayer extends Layer{
     //Martin Janda
     public PhysicalLayer physicalLayer;
+
+    public LinkLayer(){
+        physicalLayer = new PhysicalLayer();
+    }
+
+    public PhysicalLayer getPhysicalLayer() {
+        return physicalLayer;
+    }
 }
+
--- networkSimulator/src/net/sim/NetworkLayer.java
+++ networkSimulator/src/net/sim/NetworkLayer.java
@@ -3,4 +3,16 @@ package net.sim;
 public class NetworkLayer extends Layer{
     //Martin Janda
     public LinkLayer linkLayer;
+
+    public NetworkLayer(){
+        this.linkLayer = new LinkLayer();
+    }
+
+    public LinkLayer getLinkLayer() {
+        return linkLayer;
+    }
+
+    public PhysicalLayer getPhysicalLayer(){
+        return getLinkLayer().getPhysicalLayer();
+    }
 }
