7b9af0a5d4c3c4c15ecdcfc805e29b7d96313c3d
M4RS-music <65905992+M4RS-music@users.noreply.github.com>
2022-01-24 13:52:19 +0100

    Fixed Connection ID
    ID is given by simulation which interacts with Factory
    
    --Martin Janda 1/24/22

--- networkSimulator/src/net/sim/Connection.java
+++ networkSimulator/src/net/sim/Connection.java
@@ -3,19 +3,17 @@ package net.sim;
 
 public abstract class Connection implements  EventHandler{
     //Vlad & Nikita
-    private static int connectionIDIncrement = 0;
-    public int id = connectionIDIncrement++;
+    public int id;
     public Device device1;
     public Device device2;
     public ConnectionType type;
     public float failureRate;
     public int speed;
-    public Connection(Device _device1, Device _device2, ConnectionType _type){
+    public Connection(Device _device1, Device _device2, int id, ConnectionType _type){
         device1 = _device1;
         device2 = _device2;
-        id = connectionIDIncrement;
+        this.id = id;
         type = _type;
-        connectionIDIncrement++;
     }
     public void setFailureRate(float failureRate) {
         this.failureRate = failureRate;
