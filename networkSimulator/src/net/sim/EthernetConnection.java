e3ac2c200b41b7ed2b1046a122a7bd339f0dd6a2
Vladyslav48 <63408113+Vladyslav48@users.noreply.github.com>
2022-01-25 17:27:28 +0100

    Update EthernetConnection.java

--- networkSimulator/src/net/sim/EthernetConnection.java
+++ networkSimulator/src/net/sim/EthernetConnection.java
@@ -1,40 +1,32 @@
 package net.sim;
-//Vlad
+
+
 public class EthernetConnection extends Connection{
-    public int id;
-    public Device device1;
-    public Device device2;
-    public ConnectionType type;
     public float failureRate;
     public int speed;
 
 
-    public EthernetConnection(Device _device1, Device _device2, int id, ConnectionType _type) {
-        super(_device1, _device2, id, _type);
-        device1 = _device1;
-        device2 = _device2;
-        this.id = id;
-        type = _type;
-        speed = 100;
+    public EthernetConnection(Device _device1, Device _device2, int id) {
+        super(_device1, _device2, id, ConnectionType.ETHERNET);
     }
 
     public IPAddress getDevice1IP()
     {
-        IPAddress ipAddress = device1.ipAddress;
+        IPAddress ipAddress = this.device1.ipAddress;
         return ipAddress;
 
     }
 
     public IPAddress getDevice2IP()
     {
-        IPAddress ipAddress = device2.ipAddress;
+        IPAddress ipAddress = this.device2.ipAddress;
         return ipAddress;
 
     }
 
-    @Override
-    public void handleEvent(Event e) {
+    public void connectDevice(int connectionNumber, Device device){
+        ConnectionFactory.createConnection(ConnectionType.ETHERNET, connectionNumber, this.device1, this.device2);
     }
-
-
 }
+
+//Vlad
