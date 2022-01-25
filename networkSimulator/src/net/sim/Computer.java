71de559fdf30a1627f18a628af2ae14007393d2e
Lucius <arthur.s.price@protonmail.com>
2022-01-25 17:54:08 +0100

    Switch and Computed added

--- networkSimulator/src/net/sim/Computer.java
+++ networkSimulator/src/net/sim/Computer.java
@@ -1,5 +1,65 @@
 package net.sim;
 
-public class Computer extends Device{
+import java.util.HashMap;
+import java.util.Map;
+
+public class Computer extends Device {
+
+    // Computer information
+    public Map<String, String> wiredConnections = new HashMap<>();
+    public Map<String, String> wirelessConnections = new HashMap<>();
+
+    // Wired connection methods
+    public void addWiredConnection(String connectedDeviceID, String connectedDeviceIP) {
+        wiredConnections.put(connectedDeviceID, connectedDeviceIP);
+    }
+    public void delWiredConnection(String connectedDeviceID) {
+        wiredConnections.remove(connectedDeviceID);
+    }
+    public void showWiredConnections() {
+        System.out.println(this.id + " wired connections: ");
+        wiredConnections.forEach((connectedDeviceID,connectedDeviceIP)-> System.out.println("connected device ID: " + connectedDeviceID + " IP: " + connectedDeviceIP));
+    }
+
+    // Wireless connection methods
+    public void addWirelessConnection(String connectedDeviceID, String connectedDeviceIP) {
+        wirelessConnections.put(connectedDeviceID, connectedDeviceIP);
+    }
+    public void delWirelessConnection(String connectedDeviceID) {
+        wirelessConnections.remove(connectedDeviceID);
+    }
+    public void showWirelessConnections() {
+        System.out.println(this.id + " wireless connections: ");
+        wirelessConnections.forEach((connectedDeviceID,connectedDeviceIP)-> System.out.println("connected device ID: " + connectedDeviceID + " IP: " + connectedDeviceIP));
+    }
+
+    // All connection methods
+    public void showAllConnections() {
+        System.out.println(this.id + " wireless connections: ");
+        wirelessConnections.forEach((connectedDeviceID,connectedDeviceIP)-> System.out.println("connected device ID: " + connectedDeviceID + " IP: " + connectedDeviceIP));
+        System.out.println("-----");
+        System.out.println(this.id + " wired connections: ");
+        wiredConnections.forEach((connectedDeviceID,connectedDeviceIP)-> System.out.println("connected device ID: " + connectedDeviceID + " IP: " + connectedDeviceIP));
+
+    }
+
+    // IP methods
+    public void setIPAddress(String IP) {
+        this.ipAddress = new IPAddress(IP);
+    }
+    public void showIP() {
+        System.out.println("Device " + id + " IP is: " + this.ipAddress.ipStr);
+    }
+
+    // Constructor
+    public Computer(NetworkLayer _layer, DeviceType _type, int deviceID) {
+        super(_layer, _type, deviceID);
+    }
+
+    // handleEvent override
+    @Override
+    public void handleEvent(Event e) {
+
+    }
 }
-// Daniel De Corte
+// Artem Pyatigorov & Daniel De Corte
--- networkSimulator/src/net/sim/Switch.java
+++ networkSimulator/src/net/sim/Switch.java
@@ -1,5 +1,56 @@
 package net.sim;
 
-public class Switch extends Device{
+import java.util.HashMap;
+import java.util.Map;
+
+// Though in real networking switches do not use IP addresses, the simulator acts as if they do for simplicity
+public class Switch extends Device {
+
+    // Switch information
+    public Map<String, String> wiredConnections = new HashMap<>();
+    public Map<String, String> camTable = new HashMap<>();
+
+    // CAM table methods
+    public void addRoute(String packetDestination, String nextHop) {
+        camTable.put(packetDestination, nextHop);
+    }
+    public void delRoute(String packetDestination) {
+        camTable.remove(packetDestination);
+    }
+    public void showCamTable() {
+        System.out.println(this.id + " CAM table: ");
+        camTable.forEach((packetDestination,nextHop)-> System.out.println("destination: " + packetDestination + " next hop: " + nextHop));
+    }
+
+    // Wired connection methods
+    public void addConnection(String connectedDeviceID, String connectedDeviceIP) {
+        wiredConnections.put(connectedDeviceID, connectedDeviceIP);
+    }
+    public void delConnection(String connectedDeviceID) {
+        wiredConnections.remove(connectedDeviceID);
+    }
+    public void showConnections() {
+        System.out.println(this.id + " wired connections: ");
+        wiredConnections.forEach((connectedDeviceID,connectedDeviceIP)-> System.out.println("connected device ID: " + connectedDeviceID + " IP: " + connectedDeviceIP));
+    }
+
+    // IP methods
+    public void setIPAddress(String IP) {
+        this.ipAddress = new IPAddress(IP);
+    }
+    public void showIP() {
+        System.out.println("Device " + id + " IP is: " + this.ipAddress.ipStr);
+    }
+
+    // Constructor
+    public Switch(NetworkLayer _layer, DeviceType _type, int deviceID) {
+        super(_layer, _type, deviceID);
+    }
+
+    // handleEvent override
+    @Override
+    public void handleEvent(Event e) {
+
+    }
 }
-// Daniel De Corte
+// Artem Pyatigorov & Daniel De Corte
