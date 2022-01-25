09b41855a467ccf1046bb87589776a508b9b8a27
Nedr <ndrpowerr@gmail.com>
2022-01-25 21:07:27 +0300

    added addConnection

--- networkSimulator/src/net/sim/Device.java
+++ networkSimulator/src/net/sim/Device.java
@@ -1,19 +1,24 @@
 package net.sim;
 
+import java.util.LinkedList;
+
 public abstract class Device implements EventHandler {
     public int id = 0;
     public NetworkLayer layerStack;
     public DeviceType type;
     public IPAddress ipAddress;
+    public LinkedList<Connection> connections;
 
-    public Device(NetworkLayer _layer, DeviceType _type, int _deviceID, IPAddress _ip){
+    public Device(NetworkLayer _layer, DeviceType _type, int _deviceID, IPAddress _ip) {
         id = _deviceID;
         layerStack = _layer;
         type = _type;
         ipAddress = _ip;
     }
-    public Connection createConnectionFromDevice(int connectionId, ConnectionType connectionType, Device deviceTo){
-        return ConnectionFactory.createConnection(connectionType, connectionId, this, deviceTo);
+
+    public  void addConnection(Connection connection){
+        connections.add(connection);
     }
+
 }
 // Nikita Druzhkov
