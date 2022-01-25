cb0e7bf9883ad106b35465ad5f058fc354e089f7
Vladyslav48 <63408113+Vladyslav48@users.noreply.github.com>
2022-01-25 17:30:49 +0100

    Update WirelessConnection.java

--- networkSimulator/src/net/sim/WirelessConnection.java
+++ networkSimulator/src/net/sim/WirelessConnection.java
@@ -1,4 +1,31 @@
 package net.sim;
-
+//Vlad
 public class WirelessConnection extends Connection{
+    public float failureRate;
+    public int speed;
+    public int channel;
+
+
+    public WirelessConnection(Device _device1, Device _device2, int id) {
+        super(_device1, _device2, id, ConnectionType.WIRELESS);
+    }
+
+    public IPAddress getDevice1IP()
+    {
+        IPAddress ipAddress = this.device1.ipAddress;
+        return ipAddress;
+
+    }
+
+    public IPAddress getDevice2IP()
+    {
+        IPAddress ipAddress = this.device2.ipAddress;
+        return ipAddress;
+
+    }
+
+    public void connectDevice(int connectionNumber, Device device){
+        ConnectionFactory.createConnection(ConnectionType.WIRELESS, connectionNumber, this.device1, this.device2);
+    }
+
 }
