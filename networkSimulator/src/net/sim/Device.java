6a97cdd1ee24ed87eff4661deda5cf5f7f82a002
M4RS-music <65905992+M4RS-music@users.noreply.github.com>
2022-01-25 19:32:56 +0100

    Added methods for handling SimplePing (PingEvent)
    Created methods in device for dealing with ping eventds
    
    --Martin Janda 7-7:30 1/25/22

--- networkSimulator/src/net/sim/Device.java
+++ networkSimulator/src/net/sim/Device.java
@@ -20,5 +20,41 @@ public abstract class Device implements EventHandler {
         connections.add(connection);
     }
 
+    /**
+     * Method for handling a PingEvent
+     * @param event must be a PingEvent
+     */
+    public void handleSimplePing(PingEvent event){
+        Device device1 = connections.getFirst().device1;
+        Device device2 = connections.getFirst().device2;
+
+        if(device1 == this){
+            device2.acknowledgePing(event.pingSourceIP, event.pingDestinationIP, event.pingMessage, device1);
+        } else{
+            device1.acknowledgePing(event.pingSourceIP, event.pingDestinationIP, event.pingMessage, device2);
+        }
+    }
+
+    /**
+     * Prints out a simplePing recieved
+     * @param pingSourceIP
+     * @param pingDestinationIP
+     * @param pingMessage
+     */
+    private void acknowledgePing(String pingSourceIP, String pingDestinationIP, String pingMessage,Device device2) {
+        System.out.print("Device: ");
+        System.out.print(this.id);
+        System.out.println();
+        System.out.print(pingDestinationIP);
+        System.out.print(": ");
+        System.out.print("RECIEVED PING FROM: ");
+        System.out.print(pingSourceIP);
+        System.out.print("Device: ");
+        System.out.println(device2.id);
+        System.out.println("Message: ");
+        System.out.println(pingMessage);
+
+    }
+
 }
 // Nikita Druzhkov
