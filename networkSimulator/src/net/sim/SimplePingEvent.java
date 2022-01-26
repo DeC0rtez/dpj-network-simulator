f7fe33b7d02a35e17302db3f8d039d7452e9ef3e
M4RS-music <65905992+M4RS-music@users.noreply.github.com>
2022-01-26 14:38:45 +0100

    Doccumented SimplePingEvent
    --Martin Janda 1/26/22 2:20-2:40

--- networkSimulator/src/net/sim/SimplePingEvent.java
+++ networkSimulator/src/net/sim/SimplePingEvent.java
@@ -1,5 +1,11 @@
 package net.sim;
 
+/**
+ * Class dedicated to representing a SimplePingEvent
+ * Create one of these using the constructor and pass it to Simulation.scheduleEvent() to schedule a ping
+ *
+ * @author Daniel De Corte
+ */
 public class SimplePingEvent extends Event {
     public String pingSourceIP;
     public int id;
@@ -7,6 +13,12 @@ public class SimplePingEvent extends Event {
     public String pingMessage;
     public EventType type;
 
+    /**
+     * Constructor of SimplePingEvent
+     * Tells a device of a certain ID to ping the first device it has been connected to
+     * @param _handler Device you wish this event to be handled by
+     * @param id the ID of the Device you wish to send the simple ping from
+     */
     public SimplePingEvent(EventHandler _handler, int id) {
         super(_handler);
         this.id = id;
