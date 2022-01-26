bff4cfffb100013f4063dd3f2f7d239ba90433a0
M4RS-music <65905992+M4RS-music@users.noreply.github.com>
2022-01-26 14:43:02 +0100

    Doccumented EventScheduler
    --Martin Janda 1/26/22

--- networkSimulator/src/net/sim/EventScheduler.java
+++ networkSimulator/src/net/sim/EventScheduler.java
@@ -2,12 +2,29 @@ package net.sim;
 
 import java.util.LinkedList;
 
+/**
+ * Class that handles the scheduling of events
+ *
+ * @author Nikita Druzhkov
+ */
 public class EventScheduler {
     public LinkedList<Event> eventQueue = new LinkedList<Event>();
 
+    /**
+     * Add an  event to the queue
+     * Only to be used by Simulation
+     * Use Simulation.scheduleEvent()
+     * @param e
+     */
     public void schedule(Event e){
         eventQueue.add(e);
     }
+
+    /**
+     * Return the next event and remove it from the queue
+     * Only to be used by Simulation
+     * @return
+     */
     public Event next(){
         Event currentEvent = eventQueue.removeFirst();
         currentEvent.eventHandler.handleEvent(currentEvent);
