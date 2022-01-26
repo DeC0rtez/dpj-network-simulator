5510ed11d2aacbf30f568b70c89e3f157708ae0f
M4RS-music <65905992+M4RS-music@users.noreply.github.com>
2022-01-26 14:40:39 +0100

    Doccumented Event
    --Martin Janda 1/26/22

--- networkSimulator/src/net/sim/Event.java
+++ networkSimulator/src/net/sim/Event.java
@@ -1,5 +1,10 @@
 package net.sim;
 
+/**
+ * Abstract representation of an event
+ *
+ * @author Nikita Druzhkov
+ */
 public abstract  class Event {
     public EventHandler eventHandler;
     public EventType type;
