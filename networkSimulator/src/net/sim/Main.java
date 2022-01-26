a81c3989d2a1c85063c4312d38a7f3226dbd933a
M4RS-music <65905992+M4RS-music@users.noreply.github.com>
2022-01-26 13:04:52 +0100

    Comments to testing code in main
    --Martin Janda 1/26/22

--- networkSimulator/src/net/sim/Main.java
+++ networkSimulator/src/net/sim/Main.java
@@ -2,14 +2,14 @@ package net.sim;
 public class Main {
 
     public static void main(String[] args) {
-        Simulation testSimulation = new Simulation();
-        testSimulation.addDevice(DeviceType.ROUTER, new IPAddress("1.1.1.1"));
+        Simulation testSimulation = new Simulation(); //Initialize a simulation environment
+        testSimulation.addDevice(DeviceType.ROUTER, new IPAddress("1.1.1.1")); //Add a device to the simulation
         testSimulation.addDevice(DeviceType.COMPUTER, new IPAddress("1.1.1.2"));
-        Device d = testSimulation.getDeviceByID(0);
-        Device d1 = testSimulation.getDeviceByID(1);
-        System.out.println(d.type);
-        testSimulation.addConnection(d, d1, ConnectionType.ETHERNET);
-        testSimulation.printTopology();
+        Device d = testSimulation.getDeviceByID(0); //Get the first device added
+        Device d1 = testSimulation.getDeviceByID(1); //Get the seccond device added
+        System.out.println(d.type);  //Check type of device
+        testSimulation.addConnection(d, d1, ConnectionType.ETHERNET); //Make a connection between the two devices
+        testSimulation.printTopology(); //Print the stored topology
 
     }
 }
